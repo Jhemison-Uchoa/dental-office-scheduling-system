6dbc39ae9753827ce2580b399236863de11126f2
Jhemison Uchoa <jhemisonuchoa2@gmail.com>
2026-01-14 13:53:42 -0300

    Modify database connection string for security
    Updated database connection string to use placeholders for credentials.

--- database/connection.py
+++ database/connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Connects the SQLAlchemy library to the database.
-engine = create_engine('postgresql+psycopg://postgres:jhemison@localhost:5432/dentist_clinic')
+engine = create_engine('postgresql+psycopg://?????:????@localhost:5432/dentist_clinic')
 
 # By default, it creates 5 connection pools.
 session_local = sessionmaker(bind = engine, autoflush = False, autocommit = False)
