74914bc31cfaa81badc939227bcedc6872486a2d
Jhemison Uchoa <jhemisonuchoa2@gmail.com>
2026-01-22 13:38:52 -0300

    Use environment variable for database password
    Updated database connection string to use environment variable for password.

--- database/connection.py
+++ database/connection.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+# define an environment variable
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 # Connects the SQLAlchemy library to the database.
-engine = create_engine('postgresql+psycopg://?????:????@localhost:5432/dentist_clinic')
+engine = create_engine(f'postgresql+psycopg://postgres:{DB_PASSWORD}@localhost:5432/dentist_clinic')
 
 # By default, it creates 5 connection pools.
 session_local = sessionmaker(bind = engine, autoflush = False, autocommit = False)
